048e90eff1c41afa67a89611f777befe5d8392d0
ISAAC Chen <isaacchenoc@gmail.com>
2020-01-05 16:36:32 +1100

    files uploaded

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Arduino-remote-car
+
+Arduino for remote control car. 
+
+This program is implemented for a Bluetooth remote control car using Arduino board. 
+This implementation has two modes. One is Bluetooth remote controlled and the other is autonomous, which can solve a simple maze.
+
+## What you need
+1. Arduino uno
+2. Ultrasonic Module HC-SR04 HC-SR04P Distance Measuring Sonar Sensor for Arduino x 2
+3. Bluetooth module for Arduino
+4. Wires
+5. Arduino breadboard
+
+Note that to build a remote control car, you would also need wheels, chassis and batteries. 
+
+## How to run
+1. Install the software Arduino from https://www.arduino.cc/
+2. Install library NewPing in Arduino
+3. Connect Arduino uno to computer
+4. run the program in Arduino
+5. Wire all the components (you would need to check the program to see which pin to connect)
+6. you would also need Arduino bluetooth controller on your phone to controller the car. 
+
+![the_car](https://i.imgur.com/lA6nnZg.jpg)
---
+++ bt_car.ino
@@ -0,0 +1,167 @@
+#include <NewPing.h>
+#include <SoftwareSerial.h>
+SoftwareSerial BT(10, 11);
+
+
+const int trigA = 7;
+const int echoA = 8;
+const int trigB = 12;
+const int echoB = 13;
+const int motorAPin1 = 3;
+const int motorAPin2 = 5;
+const int motorBPin1 = 4;
+const int motorBPin2 = 6;
+const int maxDistance = 50;
+NewPing sonarA(trigA, echoA, maxDistance);
+NewPing sonarB(trigB, echoB, maxDistance);
+unsigned int distA;
+unsigned int distB;
+char c = 'D';
+
+void setup() {
+ BT.begin(9600);
+ Serial.begin(9600);
+  pinMode(motorAPin1, OUTPUT); 
+  pinMode(motorAPin2, OUTPUT);  
+  pinMode(motorBPin1, OUTPUT); 
+  pinMode(motorBPin2, OUTPUT);   
+  pinMode(trigA, OUTPUT);
+  pinMode(echoA, INPUT);
+  pinMode(trigB, OUTPUT);
+  pinMode(echoB, INPUT);
+}
+void(* reset) (void) = 0;
+
+void loop() {
+
+  while (BT.available()){  
+  delay(10); 
+  c = BT.read(); 
+  
+
+Serial.println(c);
+
+ if(c == '8')
+  {
+  Move();
+  delay(100);
+  break;
+  }
+
+  else if(c=='2')
+  {
+  Back();
+  delay(100);
+  break;
+  }
+
+  else if (c=='6')
+  {
+  Rturn();
+  delay(100);
+  break; 
+  }
+
+ else if (c=='4')
+ {
+  Lturn();
+  delay(100);
+  break;
+ }
+
+ else if (c=='0')
+ {
+  Stop();
+  delay(100);
+  break;
+ }
+
+ else if (c=='C')
+ {
+  reset();
+  break;  
+ }
+
+  }
+
+
+  
+while (!BT.available()){
+  if(c!='D'){break;}
+  else if (c == 'D'){
+distA = sonarA.ping_cm();
+distB = sonarB.ping_cm();
+Serial.println(distA);
+Serial.println(distB);
+
+if (distB>=8){
+Rturn();
+delay(400);
+}
+
+else if (distA>=5 && distB>=5 ) {
+ Move();
+ delay(500);
+ }
+ 
+else if (distA<2 && distB<2){
+  Back();
+delay(1000);
+}
+
+else if (distA>=8) {
+Lturn();
+delay(400);
+}
+
+else {
+  Lturn();
+  delay(100);
+  Rturn();
+  delay(100);
+  }
+
+
+}}}
+ 
+void Move(){
+    digitalWrite(motorAPin1, HIGH);
+    digitalWrite(motorAPin2, LOW);
+    digitalWrite(motorBPin1,HIGH);
+    digitalWrite(motorBPin2,LOW);
+ 
+}
+
+void Lturn(){    
+    digitalWrite (motorAPin1,HIGH);
+    digitalWrite (motorAPin2,LOW);
+    digitalWrite (motorBPin1,LOW);
+    digitalWrite (motorBPin2,HIGH);
+   
+  }
+
+void Rturn(){
+    digitalWrite(motorAPin1, LOW);
+    digitalWrite(motorAPin2, HIGH);
+    digitalWrite(motorBPin1,HIGH);
+    digitalWrite(motorBPin2,LOW);
+ 
+}
+
+void Back(){    
+    digitalWrite (motorAPin1,LOW);
+    digitalWrite (motorAPin2,HIGH);
+    digitalWrite (motorBPin1,LOW);
+    digitalWrite (motorBPin2,HIGH);
+   
+  }
+
+void Stop(){    
+    digitalWrite (motorAPin1,LOW);
+    digitalWrite (motorAPin2,LOW);
+    digitalWrite (motorBPin1,LOW);
+    digitalWrite (motorBPin2,LOW);
+}
+
+
+
